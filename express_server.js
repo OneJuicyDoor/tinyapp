52bbba27ccc42d05337afa47b39b103eb14c78e5
@OneJuicyDoor <shephardcam@gmail.com>
2023-07-06 19:14:33 -0300

    fixes editing urls, adds banner to register and login, fixed logout button, fixed styling of login and register

--- express_server.js
+++ express_server.js
@@ -10,6 +10,17 @@ const generateRandomString = function() {
   return randomString;
 };
 
+function authenticateUser(email, password) {
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email && bcrypt.compareSync(password, user.hashedPassword)) {
+      return userId;
+    }
+  }
+  return false;
+}
+
+const bcrypt = require("bcryptjs");
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -20,69 +31,102 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const users = {}; // User database
-
-const urlDatabase = {
-  b2xVn2: 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+const users = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'example@example.com',
+    hashedPassword: 'exampleHashedPassword'
+  }
 };
 
-// Delete URL endpoint
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
 app.post('/urls/:id/delete', (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect('http://localhost:8080/urls');
 });
 
-// Login endpoint
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const user = getUserByEmail(email, users);
 
-  if (user && user.password === password) {
-    res.cookie('userRandomID', user.id);
+  if (user && bcrypt.compareSync(password, user.hashedPassword)) {
+    const userId = user.id;
+    const hashedUserId = bcrypt.hashSync(userId, 10);
+    res.cookie('userRandomID', userId);
     res.redirect('/urls');
   } else {
     res.sendStatus(400);
   }
 });
 
-// Logout endpoint
+app.get('/login', (req, res) => {
+  const userId = req.cookies.userRandomID;
+  const user = users[userId];
+
+  if (user) {
+    res.redirect("/urls");
+  } else {
+    const templateVars = {
+      user: null
+    };
+    res.render('urls_loginpage.ejs', templateVars);
+  }
+});
+
+
 app.post('/logout', (req, res) => {
   res.clearCookie('userRandomID');
   res.redirect('/urls');
 });
 
-// Edit URL endpoint
 app.post('/urls/:id/edit', (req, res) => {
   const id = req.params.id;
   const newURL = req.body.longURL;
-  urlDatabase[id] = newURL;
+  urlDatabase[id].longURL = newURL;
   res.redirect(`/urls/${id}`);
 });
 
-// Create URL endpoint
 app.post('/urls', (req, res) => {
+  const userId = req.cookies.userRandomID;
+  let user = users[userId];
   console.log(req.body);
   const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
+  urlDatabase[id] = {
+    longURL: req.body.longURL,
+    userID: userId
+  };
   res.redirect(`/urls/${id}`);
 });
 
-// Registration page
 app.get('/register', (req, res) => {
-  res.render('urls_registration');
+  const userId = req.cookies.userRandomID;
+  const user = users[userId];
+
+  if (user) {
+    res.redirect('/urls');
+  } else {
+    const templateVars = {
+      user: null
+    };
+    res.render('urls_registration', templateVars);
+  }
 });
 
-// Login page
-app.get('/login', (req, res) => {
-  res.render('urls_loginpage.ejs');
-});
-
-// User registration endpoint
 app.post('/register', (req, res) => {
   const { email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10)
   if (email === '' || password === '') {
     res.sendStatus(400);
     return;
@@ -99,81 +143,74 @@ app.post('/register', (req, res) => {
   const user = {
     id: userRandomID,
     email: email,
-    password: password
+    hashedPassword: hashedPassword
   };
 
   users[userRandomID] = user;
   res.cookie('userRandomID', userRandomID);
-  console.log('astring', users);
   res.redirect('/urls');
 });
 
-// Redirect to longURL when accessing the shortURL
 app.get('/u/:id', (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id];
-  res.redirect(longURL);
+  const url = urlDatabase[id];
+  if (!url) {
+    res.status(404).send('URL not found');
+    return;
+  }
+  res.redirect(url.longURL);
 });
 
-// Create new URL page
 app.get('/urls/new', (req, res) => {
   const userId = req.cookies.userRandomID;
   const user = users[userId];
-
+  if (!userId) {
+    res.redirect("/login");
+    return;
+  }
   const templateVars = {
     user: user
   };
   res.render('urls_new', templateVars);
 });
 
-// URL index page
 app.get('/urls', (req, res) => {
   const userId = req.cookies.userRandomID;
   const user = users[userId];
+  if (!userId) {
+    res.redirect("/login");
+    return;
+  }
+  
+  const userURLs = {};
+  for (const id in urlDatabase) {
+    if (urlDatabase[id].userID === userId) {
+      userURLs[id] = urlDatabase[id].longURL;
+    }
+  }
 
   const templateVars = {
     user: user,
-    urls: urlDatabase
+    urls: userURLs
   };
-  console.log(userId);
-  console.log(user);
   res.render('urls_index', templateVars);
 });
 
-// Show specific URL page
 app.get('/urls/:id', (req, res) => {
   const userId = req.cookies.userRandomID;
   const user = users[userId];
   const id = req.params.id;
-  const longURL = urlDatabase[id];
-
+  const url = urlDatabase[id];
+  if (!userId || !url || url.userID !== userId) {
+    res.status(404).send('URL not found');
+    return;
+  }
   const templateVars = {
     user: user,
     id: id,
-    longURL: longURL
+    longURL: url.longURL // Access the longURL property of the url object
   };
   res.render('urls_show', templateVars);
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
